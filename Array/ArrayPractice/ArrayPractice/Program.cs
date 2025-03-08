27fbc840b6f9ab894a05eea105e95b604ff976bd
Nafiz Imtiaz Khan <145887236+nafirk99@users.noreply.github.com>
2025-03-08 19:51:35 +0600

    Array, Reflection

--- Array/ArrayPractice/ArrayPractice/Program.cs
+++ Array/ArrayPractice/ArrayPractice/Program.cs
@@ -1,43 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
+using ArrayPractice;
+
+//int[] a = new int[2];
+
+//a[0] = 2; 
+
+//a[1] = 3;
+
+//for (int i = 0; i < a.Length; i++)
+//{
+//    //Console.WriteLine(a[i]);
+//}
+
+//int[] b = new int[] { 2, 3, 4, 5, 6 ,2,9,2,1,0};
+
+//foreach (int i in b)
+//{
+//    Console.WriteLine(i);
+//}
+
+//for(int i=0; i<b.Length; i++)
+//{
+//    Console.WriteLine($"B using for : {b[i]}");
+//}
+//foreach (int i in b)
+//{
+//    Console.WriteLine($"b foreach: {i}");
+//}
+
+//Array.Sort(b);
+//Console.WriteLine("Sorted:");
+//foreach (int i in b)
+//{
+//    Console.WriteLine(i + " ");
+//}
+//Array.Reverse(b);
+//Console.WriteLine("Reversed: ");
+//foreach (int i in b)
+//{
+//    Console.WriteLine(i + " ");
+//}
 
 
+//var p = new Person()
+//{
+//    Id = 1,
+//    Name = "Test",
+//};
 
-int[] a = new int[2];
+//Type type = typeof(Person);
+//Type type2 = p.GetType();
 
-a[0] = 2; 
+//Assembly r = Assembly.GetExecutingAssembly();
 
-a[1] = 3;
 
-for (int i = 0; i < a.Length; i++)
+string Path = "F:\\Csharp_CP_DSA_Practice\\Array\\ArrayPractice\\DemoLibrary\\bin\\Debug\\net8.0\\DemoLibrary.dll";
+Assembly F = Assembly.LoadFile(Path);
+
+Type[] Rtypes = F.GetTypes();
+
+for (int i = 0; i< Rtypes.Length; i++)
 {
-    //Console.WriteLine(a[i]);
+    Console.WriteLine(Rtypes[i].Name);
 }
 
-int[] b = new int[] { 2, 3, 4, 5, 6 ,2,9,2,1,0};
-
-foreach (int i in b)
-{
-    Console.WriteLine(i);
-}
-
-for(int i=0; i<b.Length; i++)
-{
-    Console.WriteLine($"B using for : {b[i]}");
-}
-foreach (int i in b)
-{
-    Console.WriteLine($"b foreach: {i}");
-}
-
-Array.Sort(b);
-Console.WriteLine("Sorted:");
-foreach (int i in b)
-{
-    Console.WriteLine(i + " ");
-}
-Array.Reverse(b);
-Console.WriteLine("Reversed: ");
-foreach (int i in b)
-{
-    Console.WriteLine(i + " ");
-}
+Console.WriteLine($"Tiple F length: {Rtypes.Length}");
